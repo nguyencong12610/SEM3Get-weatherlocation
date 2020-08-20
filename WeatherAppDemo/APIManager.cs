d3f827fc1a3fa91c95f942878a820ddde375ceda
nguyencong12610 <53223955+nguyencong12610@users.noreply.github.com>
2020-08-20 20:40:33 +0700

    pust

---
+++ WeatherAppDemo/APIManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace WeatherAppDemo
+{
+    class APIManager
+    {
+        public async static Task<RootObject> GetWeather(double lat, double lon)
+        {
+
+            var http = new HttpClient();
+            var url = String.
+                Format("http://api.openweathermap.org/data/2.5/weather?lat=21&lon=105.804817&units=metric&appid=96381a872b1b405c5bf83b2ed63d9561", lat, lon);
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+
+            var serializer = new DataContractSerializer(typeof(RootObject));
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+
+            var data = (RootObject)serializer.ReadObject(ms);
+            return data;
+
+        }
+        class LocationData
+        {
+            public class Coord
+            {
+                public double lon { get; set; }
+                public int lat { get; set; }
+            }
+
+            public class Weather
+            {
+                public int id { get; set; }
+                public string main { get; set; }
+                public string description { get; set; }
+                public string icon { get; set; }
+            }
+
+            public class Main
+            {
+                public int temp { get; set; }
+                public double feels_like { get; set; }
+                public int temp_min { get; set; }
+                public int temp_max { get; set; }
+                public int pressure { get; set; }
+                public int humidity { get; set; }
+            }
+
+            public class Wind
+            {
+                public double speed { get; set; }
+                public int deg { get; set; }
+            }
+
+            public class Clouds
+            {
+                public int all { get; set; }
+            }
+
+            public class Sys
+            {
+                public int type { get; set; }
+                public int id { get; set; }
+                public string country { get; set; }
+                public int sunrise { get; set; }
+                public int sunset { get; set; }
+            }
+
+            public class RootObject
+            {
+                public Coord coord { get; set; }
+                public List<Weather> weather { get; set; }
+                public string @base { get; set; }
+                public Main main { get; set; }
+                public int visibility { get; set; }
+                public Wind wind { get; set; }
+                public Clouds clouds { get; set; }
+                public int dt { get; set; }
+                public Sys sys { get; set; }
+                public int timezone { get; set; }
+                public int id { get; set; }
+                public string name { get; set; }
+                public int cod { get; set; }
+
+            }
+
+        }
+    }
+}
